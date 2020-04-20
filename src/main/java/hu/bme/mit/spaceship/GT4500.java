1c2b536e533e7649b1f0e33252dcc8da47288a34
Kristóf <kaszalakristof1@gmail.com>
2020-04-20 17:27:54 +0200

    Branch B changes

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -83,13 +83,8 @@ public class GT4500 implements SpaceShip {
             firingSuccess = primaryTorpedoStore.fire(1);
             firingSuccess = secondaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = false;
+            //poooog
         }
-    	
-    	
-    	
-    	
-    	
-    	//5HEAD
         break;
     }
 
